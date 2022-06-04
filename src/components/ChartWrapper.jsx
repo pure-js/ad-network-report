cbad3753fc44058f9ad6837518504b1040f254dd
pure-js <3690368+pure-js@users.noreply.github.com>
2022-06-05 03:35:51 +0400

    feat: add daily revenue chart

---
+++ src/components/ChartWrapper.jsx
@@ -0,0 +1,68 @@
+import {
+  Chart,
+  LineElement,
+  PointElement,
+  CategoryScale,
+  LinearScale,
+  Legend,
+  Tooltip,
+} from 'chart.js';
+import { Line } from 'react-chartjs-2';
+
+Chart.register(
+  LineElement,
+  PointElement,
+  CategoryScale,
+  LinearScale,
+  Legend,
+  Tooltip,
+);
+
+const options = {
+  responsive: true,
+  plugins: {
+    legend: {
+      position: 'top',
+    },
+  },
+};
+
+
+function ChartWrapper({ data }) {
+  const labels = Object.keys(data).slice(0, -1);
+  let ApplovinLabel = [];
+  let FacebookLabel = [];
+  let GoogleAdsLabel = [];
+  labels.map((date) => {
+    ApplovinLabel.push(data[date].Applovin.daily_revenue);
+    FacebookLabel.push(data[date].Facebook.daily_revenue);
+    GoogleAdsLabel.push(data[date].GoogleAds.daily_revenue);
+  });
+
+  const chartData = {
+    labels,
+    datasets: [
+      {
+        label: 'Applovin',
+        data: ApplovinLabel,
+        borderColor: '#d63964',
+      },
+      {
+        label: 'Facebook',
+        data: FacebookLabel,
+        borderColor: '#46a3e8',
+      },
+      {
+        label: 'GoogleAds',
+        data: GoogleAdsLabel,
+        borderColor: '#f6c244',
+      },
+    ],
+  };
+
+  return (
+    <Line options={options} data={chartData} />
+  )
+}
+
+export default ChartWrapper;
