e989817ab63b6e1f600e58f6b3e344d1ac7a3189
pure-js <3690368+pure-js@users.noreply.github.com>
2023-08-28 21:50:04 +0400

    build: add visualizer plugin

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,16 @@
-import { defineConfig } from 'vite';
+import { defineConfig, type PluginOption } from 'vite';
 import react from '@vitejs/plugin-react';
+import { visualizer } from 'rollup-plugin-visualizer';
 import { fileURLToPath, URL } from 'url';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    visualizer({
+      template: 'raw-data',
+    }) as PluginOption,
+  ],
   resolve: {
     alias: [
       {
