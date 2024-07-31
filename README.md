8b0e68646582d8c55c46c3c202c5e4e0cbbf4f28
Arthur Green <curious.dev@icloud.com>
2024-08-01 01:07:10 +0400

    fix: msw

--- README.md
+++ README.md
@@ -2,4 +2,4 @@
 
 ## The Task
 
-Using the JSON data sample lockated at `src/mocks/networks.json` below, please implement a new responsive web page allowing to visualize and interact with a data table, as well as 2 charts (spend + revenue), providing a smooth user experience. Allowed JS plugins / libraries: Pure JS / ReactJS / DataTable / ChartJS
+Using the JSON data sample lockated at `src/mocks/networks.json` below, please implement a new responsive web page that allows you to visualize and interact with a data table, as well as 2 charts (spend + revenue), providing a smooth user experience. Allowed JS plugins / libraries: ReactJS / DataTable / ChartJS
--- src/main.tsx
+++ src/main.tsx
@@ -1,8 +1,9 @@
-import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
-import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
+
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 
 import App from './App';
 import './index.scss';
@@ -11,7 +12,11 @@ import Home from './pages/Home';
 import Report from './pages/report';
 import ReportOverview from './pages/report/Overview';
 
-worker.start();
+worker.start({
+  serviceWorker: {
+    url: '/ad-network-report/mockServiceWorker.js',
+  },
+});
 const queryClient = new QueryClient();
 
 const domNode = document.getElementById('app')!;
