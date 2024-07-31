e9e9c91ac54a9b4524c81db9b4bd2b54c36b5bb4
Arthur Green <curious.dev@icloud.com>
2024-07-31 21:12:00 +0400

    ci: add deploy action

--- src/main.tsx
+++ src/main.tsx
@@ -1,16 +1,15 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
-
-import { worker } from './mocks/browser';
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 
 import App from './App';
+import './index.scss';
+import { worker } from './mocks/browser';
 import Home from './pages/Home';
 import Report from './pages/report';
 import ReportOverview from './pages/report/Overview';
-import './index.scss';
 
 worker.start();
 const queryClient = new QueryClient();
@@ -19,7 +18,7 @@ const domNode = document.getElementById('app')!;
 ReactDOM.createRoot(domNode).render(
   <React.StrictMode>
     <QueryClientProvider client={queryClient}>
-      <BrowserRouter>
+      <BrowserRouter basename="/ad-network-report">
         <Routes>
           <Route path="/" element={<App />}>
             <Route index element={<Home />} />
